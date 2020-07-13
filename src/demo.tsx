5110ed4aa254861155113b7ee7bed15d11f20c9c
Clara Ruiz Fernandez <clara.ruizfernandez@tui.be>
2020-07-13 12:00:53 +0200

    React useEffect basic example

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,30 +1,17 @@
 import React from "react";
 
-interface UserInfo{
-    name: string,
-    lastname: string
-};
 
 export const MyComponent: React.FC = () => {
-    const [userInfo, setUserInfo] = React.useState<UserInfo>({
-        name: "John",
-        lastname: "Doe"
-    })
+    const [username, setUserName] = React.useState("");
+
+    React.useEffect(() => {
+        setUserName("John");
+    }, []);
+    
     return (
         <>
-            <h4>{`${userInfo.name} ${userInfo.lastname}`}</h4>
-            <input value={userInfo.name} onChange={e => 
-            setUserInfo({
-                ...userInfo, 
-                name:e.target.value,
-            })
-            } />
-             <input value={userInfo.lastname} onChange={e => 
-            setUserInfo({
-                ...userInfo, 
-                lastname:e.target.value,
-            })
-            } />
+            <h4>{username}</h4>
+            <input value={username} onChange={e => setUserName(e.target.value)} />            
         </>
     );
 };
